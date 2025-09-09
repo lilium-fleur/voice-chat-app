d585eaa0ca6a083ef5ed316159bb6dfeabcad64d
haruxime <haruxime01@gmail.com>
2025-09-09 18:26:12 +0400

    Init

---
+++ src/main/java/org/fleur/voicechatapp/VoiceChatAppApplication.java
@@ -0,0 +1,13 @@
+package org.fleur.voicechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceChatAppApplication.class, args);
+    }
+
+}
